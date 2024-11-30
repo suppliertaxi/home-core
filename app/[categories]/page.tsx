1d3a66b12ccba6984aa553c52b78e73cc6bec569
Dheeraj <sahdheeraj518@gmail.com>
2024-11-30 20:28:09 +0530

    fix: lint error

--- app/[categories]/page.tsx
+++ app/[categories]/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import React from "react";
 import CementPage from "../_common/components/cement-page";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
-function CategoriesPage({ params }: { params: { categories: string } }) {
-  console.log("object", params.categories);
-  if (params.categories === "cement") {
+function CategoriesPage() {
+  const param = useParams();
+  const categories = param?.categories;
+  if (categories === "cement") {
     return (
       <div>
         <CementPage />
